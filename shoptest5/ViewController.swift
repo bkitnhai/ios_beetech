09930fcd016440a4b182f237035a67867c013afd
NguyenHai <nguyenhai@NguyenHais-Mac.local>
2016-11-29 13:47:16 +0100

    Initial Commit

---
+++ shoptest5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shoptest5
+//
+//  Created by NguyenHai on 11/29/16.
+//  Copyright © 2016 NguyenHai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
